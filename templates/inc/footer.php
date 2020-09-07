3dbde2e3610267a15ef0d24fcc8e0fc0d20835b2
esdu <esdu@Esads-MBP.fritz.box>
2020-09-07 17:29:39 +0200

    cdn and styles added, header&footer&database files etc created

---
+++ templates/inc/footer.php
@@ -0,0 +1,6 @@
+        <footer class="footer">
+            <p>© 2020 Joblister, Inc.</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ templates/inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JobLister</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills justify-content-end">
+            <li class="nav-item mr-3" role="presentation"><a class="text-decoration-none" href="index.php">Home</a></li>
+            <li class="nav-item mr-3" role="presentation"><a class="text-decoration-none" href="create.php">Create Listing</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted"><?php echo SITE_TITLE; ?></h3>
+    </div>
